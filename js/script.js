ff2d34f891c3d20542d78084ea4b4a90c3a63fbd
balbo97 <balbosauro1@gmail.com>
2023-11-29 15:02:28 +0100

    PARI O DISPARI completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// 1- PALINDROMA 
+
 // RICHIEDO ALL'UTENTE DI INSERIRE UNA PAROLA 
 let world = prompt('inserisci una parola')
 
@@ -17,3 +19,43 @@ function palindroma(x){
 
 // UTILIZZO LA FUNZIONE 
 palindroma(world);
+
+//------------------------------------------------------------------------------------------//
+
+// 2- PARI O DISPARI 
+
+// CHIEDO ALL'UTENTE DI INSERIRE PARI O DISPARI 
+let even_odd = prompt('inserisci pari o dispari');
+console.log(even_odd);
+
+// CHIEDO ALL'UTENTE DI INSERIRE UN NUMERO DA UNO A CINQUE 
+let player_number = parseInt(prompt('inserisci un numero da uno a cinque'));
+console.log(player_number)
+
+// CREO UNA FUNZIONE PER CREARE UN NUMERO RANDOM DA 1 A 5 
+function randomNumber (){
+   return Math.floor(Math.random()*5 + 1)
+}
+let computer_number = randomNumber()
+console.log(computer_number)
+
+// CREO UNA FUNZIONE CHE SOMMI I DUE NUMERI E CONTROLLI SE SIANO PARI O DISPARI 
+function somma(x,y){
+    
+    if((x + y)%2 == 0){
+        return 'pari';
+    }
+    return 'dispari' 
+}
+
+let result = somma(player_number, computer_number);
+console.log(result)
+
+
+// CONFRONTO IL RISULTATO FINALE CON LA SCELTA DELL'UTENTE E DICHIARO CHI HA VINTO 
+if (result == even_odd){
+    console.log('hai vinto')
+}
+else{
+    console.log('hai perso')
+}
